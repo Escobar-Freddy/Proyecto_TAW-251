e6b054402b50b72a99970e78f5ae6b5ca26fe3bf
FREDDY <fescobarc@fcpn.edu.bo>
2026-06-06 23:48:12 -0400

    actualizando frontende

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000";
-
+//const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL =
+  process.env.REACT_APP_BACKEND_URL || "http://localhost:3000";
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
